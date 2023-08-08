46095e9fe893e0598fd177cbae53983572d61db0
TaeHyong Kim <kthstar27@gmail.com>
2023-08-08 20:37:06 +0900

    1.0

--- main.tf
+++ main.tf
@@ -9,52 +9,24 @@ terraform {
     }
 }
 
-resource "null_resource" "PRI_KEY_FILE_FROM_S3_TO_RUNNER" {
-    count = (var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.S3_PRI_KEY_FILE != "" ? 1 : 0)
+resource "null_resource" "EXECUTE_SCRIPT" {
+    for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
+
     triggers = {
-        always_run = timestamp()
+        always_run  = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
+        NAME        = file("${each.value.NAME}")
+        VARAIANT    = try(join(",", "${each.value.VARIANTs}"), null)
     }
 
     provisioner "local-exec" {
-        interpreter = ["bash", "-c"]
-        on_failure = continue # Add this line to ignore errors
         command = <<-EOF
-            mkdir -p "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_DIR}"
-            chmod -R 777 "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_DIR}" || true
-            if [[ ! -f "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_PRI_KEY_FILE}" ]]; then
-                aws s3 cp "s3://${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.S3_PRI_KEY_FILE}" "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_PRI_KEY_FILE}"  --profile "${var.PROFILE}"
-            fi
-            while [ ! -f "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_PRI_KEY_FILE}" ]; do
-                sleep 3
-            done
-            chmod 400 "${var.PRI_KEY_FILE_FROM_S3_TO_RUNNER.RUNNER_PRI_KEY_FILE}"
-        EOF
-    }
-}
-
-resource "null_resource" "WAIT_REMOTE_HOST_FOR_CONNECTION" {
-    count = (var.WAIT_REMOTE_HOST_FOR_CONNECTION.LOCAL_HOST_PRI_KEY_FILE != "" ? 1 : 0)
-    triggers = {
-        always_run = timestamp()
-    }
-    
-    provisioner "local-exec" {
-        interpreter = ["bash", "-c"]
-        command = <<-EOF
-        if [[ -n "${var.WAIT_REMOTE_HOST_FOR_CONNECTION.LOCAL_HOST_PRI_KEY_FILE}" ]] && [[ -n "${var.WAIT_REMOTE_HOST_FOR_CONNECTION.REMOTE_HOST_USER}" ]] && [[ -n "${var.WAIT_REMOTE_HOST_FOR_CONNECTION.REMOTE_HOST_IP}" ]]; then
-            LOCAL_HOST_PRI_KEY_FILE="${var.WAIT_REMOTE_HOST_FOR_CONNECTION.LOCAL_HOST_PRI_KEY_FILE}"
-            REMOTE_HOST_USER="${var.WAIT_REMOTE_HOST_FOR_CONNECTION.REMOTE_HOST_USER}"
-            REMOTE_HOST_IP="${var.WAIT_REMOTE_HOST_FOR_CONNECTION.REMOTE_HOST_IP}"
-            while true; do
-                if ssh -q -o "StrictHostKeyChecking=no" -o "PreferredAuthentications=publickey" -i "$LOCAL_HOST_PRI_KEY_FILE" "$REMOTE_HOST_USER@$REMOTE_HOST_IP" exit; then
-                    echo "SSH connection is now available. Remote PC has rebooted successfully."
-                    break
-                else
-                    echo "SSH connection not available yet. Waiting for 10 seconds..."
-                    sleep 10
-                fi
-            done
-        fi
+        #!/bin/bash
+        %{ if length("${each.value.VARIANTs}") > 0 ~}
+            %{ for VARIANT in "${each.value.VARIANTs}" ~}
+            export ${VARIANT}
+            %{ endfor ~}
+        %{ endif ~}
+        bash "${each.value.NAME}"
         EOF
     }
 }
--- variables.tf
+++ variables.tf
@@ -3,30 +3,11 @@ variable "PROFILE" {
     default = null
 }
 
-variable "PRI_KEY_FILE_FROM_S3_TO_RUNNER" {
-    type = object({
-        S3_PRI_KEY_FILE = string
-        RUNNER_DIR = string
-        RUNNER_PRI_KEY_FILE = string
-    })
-
-    default = {
-        S3_PRI_KEY_FILE = ""
-        RUNNER_DIR = ""
-        RUNNER_PRI_KEY_FILE = ""
-    }
-}
-
-variable "WAIT_REMOTE_HOST_FOR_CONNECTION" {
-    type = object({
-        LOCAL_HOST_PRI_KEY_FILE = string
-        REMOTE_HOST_USER = string
-        REMOTE_HOST_IP = string
-    })
-
-    default = {
-        LOCAL_HOST_PRI_KEY_FILE = ""
-        REMOTE_HOST_USER = ""
-        REMOTE_HOST_IP = ""
-    }
+variable "SCRIPTs" {
+    type = list(object({
+        ALWAYS = optional(bool)
+        NAME = string
+        VARIANTs = optional(list(string))
+    }))
+    default = []
 }
