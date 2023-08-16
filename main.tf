5dc7f27faeaa32531165ef8b677b6c484c4159f1
CAL-REPO <kthstar27@gmail.com>
2023-08-16 21:36:55 +0900

    1.0

--- main.tf
+++ main.tf
@@ -56,31 +56,3 @@ resource "null_resource" "EXECUTE_SCRIPT" {
         interpreter = ["bash", "-c"]
     }
 }
-
-# resource "null_resource" "EXECUTE_SCRIPT" {
-#     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
-
-#     triggers = {
-#         always_run      = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-#         PRE_COMMAND     = try("${each.value.PRE_COMMAND}", "")
-#         VARAIANT        = try(join(",", "${each.value.VARIANTs}"), "")
-#         NAME            = try(file("${each.value.NAME}"), null)
-#         POST_COMMAND    = try("${each.value.POST_COMMAND}", "")
-#     }
-
-#     provisioner "local-exec" {
-#         command = <<-EOF
-#         ${each.value.PRE_COMMAND}
-#         %{ if length("${each.value.VARIANTs}") > 0 ~}
-#             %{ for VARIANT in "${each.value.VARIANTs}" ~}
-#             export ${VARIANT}
-#             %{ endfor ~}
-#         %{ endif ~}
-#         %{ if "${each.value.NAME}" != null ~}
-#             bash "${each.value.NAME}"
-#         %{ endif ~}
-#         ${each.value.POST_COMMAND}
-#         EOF
-#         interpreter = ["bash", "-c"]
-#     }
-# }
