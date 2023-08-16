1f006f09742f6165d318cb3685c08e10e0694392
CAL-REPO <kthstar27@gmail.com>
2023-08-16 21:06:16 +0900

    1.0

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "template_file" "PRE_COMMAND_SCRIPT" {
     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
 
     template = <<-EOF
-    ${try("${each.value.PRE_COMMAND}", "")}
+    ${try("${base64encode("${each.value.PRE_COMMAND}")}", "")}
     EOF
 }
 
@@ -21,7 +21,7 @@ data "template_file" "POST_COMMAND_SCRIPT" {
     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
 
     template = <<-EOF
-    ${try("${each.value.POST_COMMAND}", "")}
+    ${try("${base64encode("${each.value.POST_COMMAND_SCRIPT}")}", "")}
     EOF
 }
 
@@ -29,28 +29,32 @@ resource "null_resource" "EXECUTE_SCRIPT" {
     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
 
     triggers = {
-        always_run      = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-        PRE_COMMAND     = try("${each.value.PRE_COMMAND}", "")
-        VARIANT        = try(join(",", "${each.value.VARIANTs}"), "")
-        NAME            = try(file("${each.value.NAME}"), null)
-        POST_COMMAND    = try("${each.value.POST_COMMAND}", "")
+        always_run    = try(each.value.ALWAYS == true ? timestamp() : null, null)
+        PRE_COMMAND   = try(data.template_file.PRE_COMMAND_SCRIPT[each.key].rendered, "")
+        VARIANT       = try(join(",", each.value.VARIANTs), "")
+        NAME          = try(file(each.value.NAME), null)
+        POST_COMMAND  = try(data.template_file.POST_COMMAND_SCRIPT[each.key].rendered, "")
     }
 
     provisioner "local-exec" {
-        command = <<-EOF
-        ${self.triggers.PRE_COMMAND}
-        %{ if "${self.triggers.VARIANT}" != "" ~}
-            %{ for VARIANT in split(",", "${self.triggers.VARIANT}") ~}
+    command = <<-EOF
+        %{ if self.triggers.PRE_COMMAND != "" ~}
+            echo "${self.triggers.PRE_COMMAND}" | base64 --decode | bash -s
+        %{ endif ~}
+        %{ if self.triggers.VARIANT != "" ~}
+            %{ for VARIANT in split(",", self.triggers.VARIANT) ~}
             export ${VARIANT}
             %{ endfor ~}
         %{ endif ~}
-        %{ if "${self.triggers.NAME}" != null ~}
+        %{ if self.triggers.NAME != null ~}
             bash "${self.triggers.NAME}"
         %{ endif ~}
-        ${self.triggers.POST_COMMAND}
-        EOF 
-        interpreter = ["bash", "-c"]
-    }
+        %{ if self.triggers.POST_COMMAND != "" ~}
+            echo "${self.triggers.POST_COMMAND}" | base64 --decode | bash -s
+        %{ endif ~}
+    EOF 
+    interpreter = ["bash", "-c"]
+  }
 }
 
 # resource "null_resource" "EXECUTE_SCRIPT" {
