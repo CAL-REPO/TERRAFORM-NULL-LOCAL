61509544b313d73bbcdddb1b8d291aba6a5298e2
CAL-REPO <kthstar27@gmail.com>
2023-08-16 18:13:14 +0900

    1.0

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ resource "null_resource" "EXECUTE_SCRIPT" {
 
     triggers = {
         always_run      = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-        PRE_COMMAND     = try(join(",", "${each.value.PRE_COMMAND}"), "")
+        PRE_COMMAND     = try("${each.value.PRE_COMMAND}", "")
         VARAIANT        = try(join(",", "${each.value.VARIANTs}"), [])
         NAME            = try(file("${each.value.NAME}"), null)
-        POST_COMMAND    = try(join(",", "${each.value.PRE_COMMAND}"), "")
+        POST_COMMAND    = try("${each.value.PRE_COMMAND}", "")
     }
 
     provisioner "local-exec" {
