b4267209a2ed0a487205a371a51883af8c08a0d4
CAL-REPO <kthstar27@gmail.com>
2023-08-16 19:23:13 +0900

    1.0

--- main.tf
+++ main.tf
@@ -9,6 +9,31 @@ terraform {
     }
 }
 
+# data "template_file" "SCRIPT" {
+#     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
+
+#     triggers = {
+#         always_run      = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
+#         PRE_COMMAND     = try("${each.value.PRE_COMMAND}", "")
+#         VARAIANT        = try(join(",", "${each.value.VARIANTs}"), [])
+#         NAME            = try(file("${each.value.NAME}"), null)
+#         POST_COMMAND    = try("${each.value.PRE_COMMAND}", "")
+#     }
+
+#     template = <<-EOF
+#     ${each.value.PRE_COMMAND}
+#     %{ if length("${each.value.VARIANTs}") > 0 ~}
+#         %{ for VARIANT in "${each.value.VARIANTs}" ~}
+#         export ${VARIANT}
+#         %{ endfor ~}
+#     %{ endif ~}
+#     %{ if "${each.value.NAME}" != null ~}
+#         bash "${each.value.NAME}"
+#     %{ endif ~}
+#     ${each.value.POST_COMMAND}
+#     EOF
+# }
+
 resource "null_resource" "EXECUTE_SCRIPT" {
     for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
 
@@ -33,5 +58,6 @@ resource "null_resource" "EXECUTE_SCRIPT" {
         %{ endif ~}
         ${each.value.POST_COMMAND}
         EOF
+        interpreter = ["bash", "-c"]
     }
 }
