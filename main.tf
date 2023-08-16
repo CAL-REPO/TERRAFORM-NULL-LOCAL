ad99d135d48544e9afb207b6eeac3674e7add6eb
CAL-REPO <kthstar27@gmail.com>
2023-08-16 19:48:47 +0900

    1.0

--- main.tf
+++ main.tf
@@ -28,28 +28,33 @@ data "template_file" "POST_COMMAND_SCRIPT" {
 }
 
 resource "null_resource" "EXECUTE_SCRIPT" {
-    for_each = { for index, SCRIPT in var.SCRIPTs : index => SCRIPT }
+    count = (length(var.SCRIPTs) > 0 ?
+            length(var.SCRIPTs) : 0)
 
     triggers = {
-        always_run      = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-        PRE_COMMAND_SCRIPT = "${data.template_file.SCRIPT[count.index].rendered}"
-        VARAIANT        = try(join(",", "${each.value.VARIANTs}"), [])
-        NAME            = try(file("${each.value.NAME}"), null)
-        POST_COMMAND_SCRIPT = "${data.template_file.SCRIPT[count.index].rendered}"
+        always_run      = try("${var.SCRIPTs[count.index].ALWAYS}" == true ? timestamp() : null, null)
+        PRE_COMMAND_SCRIPT = try("${data.template_file.SCRIPT[count.index].rendered}", null)
+        VARAIANT        = try(join(",", "${var.SCRIPTs[count.index].VARIANTs}"), [])
+        NAME            = try(file("${var.SCRIPTs[count.index].NAME}"), null)
+        POST_COMMAND_SCRIPT = try("${data.template_file.SCRIPT[count.index].rendered}",null)
     }
 
     provisioner "local-exec" {
         command = <<-EOF
-        bash ${data.template_file.PRE_COMMAND_SCRIPT}
-        %{ if length("${each.value.VARIANTs}") > 0 ~}
-            %{ for VARIANT in "${each.value.VARIANTs}" ~}
+        %{ if "${self.triggers.PRE_COMMAND_SCRIPT}" != null ~}
+        bash ${data.template_file.PRE_COMMAND_SCRIPT[count.index]}
+        %{ endif ~}
+        %{ if length("${self.triggers.VARIANTs}") > 0 ~}
+            %{ for VARIANT in "${var.SCRIPTs[count.index].VARIANTs}" ~}
             export ${VARIANT}
             %{ endfor ~}
         %{ endif ~}
-        %{ if "${each.value.NAME}" != null ~}
-            bash "${each.value.NAME}"
+        %{ if "${self.triggers.NAME}" != null ~}
+            bash "${var.SCRIPTs[count.index].NAME}"
         %{ endif ~}
-        bash ${data.template_file.POST_COMMAND_SCRIPT}
+        %{ if "${self.triggers.POST_COMMAND_SCRIPT}" != null ~}
+        bash ${data.template_file.POST_COMMAND_SCRIPT[count.index]}
+        %{ endif ~}
         EOF 
         interpreter = ["bash", "-c"]
     }
