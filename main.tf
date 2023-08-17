1607c1098bb36b0e493344ba1a604f99e3e15b50
CAL-REPO <kthstar27@gmail.com>
2023-08-17 23:14:19 +0900

    1.0

--- main.tf
+++ main.tf
@@ -9,57 +9,25 @@ terraform {
     }
 }
 
-data "template_file" "APPLY_PRE_COMMAND_SCRIPT" {
-    for_each = { for index, SCRIPT in var.APPLY_SCRIPTs : index => SCRIPT }
-
-    template = <<-EOF
-        ${try("${base64encode("${each.value.PRE_COMMAND}")}", "")}
-    EOF
-}
-
-data "template_file" "APPLY_POST_COMMAND_SCRIPT" {
-    for_each = { for index, SCRIPT in var.APPLY_SCRIPTs : index => SCRIPT }
-
-    template = <<-EOF
-        ${try("${base64encode("${each.value.POST_COMMAND}")}", "")}
-    EOF
-}
-
-data "template_file" "DESTROY_PRE_COMMAND_SCRIPT" {
-    for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
-
-    template = <<-EOF
-        ${try("${base64encode("${each.value.PRE_COMMAND}")}", "")}
-    EOF
-}
-
-data "template_file" "DESTROY_POST_COMMAND_SCRIPT" {
-    for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
-
-    template = <<-EOF
-        ${try("${base64encode("${each.value.POST_COMMAND}")}", "")}
-    EOF
-}
-
 resource "null_resource" "EXECUTE_APPLY_SCRIPT" {
     for_each = { for index, SCRIPT in var.APPLY_SCRIPTs : index => SCRIPT }
 
     triggers = {
         always_run    = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-        PRE_COMMAND   = try(data.template_file.APPLY_PRE_COMMAND_SCRIPT[each.key].rendered, "")
+        PRE_COMMAND   = try("${each.value.PRE_COMMAND}", "")
         VARIANT       = try(join(",", "${each.value.VARIANTs}"), "")
         NAME          = try(file("${each.value.NAME}"), null)
-        POST_COMMAND  = try(data.template_file.APPLY_POST_COMMAND_SCRIPT[each.key].rendered, "")
+        POST_COMMAND  = try("${each.value.POST_COMMAND}", "")
     }
 
     provisioner "local-exec" {
         interpreter = ["bash", "-c"]
         command = <<-EOF
             %{ if self.triggers.PRE_COMMAND != "" ~}
-                echo "${self.triggers.PRE_COMMAND}" | base64 --decode | bash -s
+                ${"${self.triggers.PRE_COMMAND}"}
             %{ endif ~}
             %{ if self.triggers.VARIANT != "" ~}
-                %{ for VARIANT in split(",", self.triggers.VARIANT) ~}
+                %{ for VARIANT in split(",", "${self.triggers.VARIANT}") ~}
                 export ${VARIANT}
                 %{ endfor ~}
             %{ endif ~}
@@ -67,41 +35,57 @@ resource "null_resource" "EXECUTE_APPLY_SCRIPT" {
                 bash "${self.triggers.NAME}"
             %{ endif ~}
             %{ if self.triggers.POST_COMMAND != "" ~}
-                echo "${self.triggers.POST_COMMAND}" | base64 --decode | bash -s
+                ${"${self.triggers.POST_COMMAND}"}
             %{ endif ~}
         EOF 
     }
 }
 
-resource "null_resource" "EXECUTE_DESTROY_SCRIPT" {
-    for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
+# data "template_file" "DESTROY_PRE_COMMAND_SCRIPT" {
+#     for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
 
-    triggers = {
-        always_run    = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
-        PRE_COMMAND   = try(data.template_file.DESTROY_PRE_COMMAND_SCRIPT[each.key].rendered, "")
-        VARIANT       = try(join(",", "${each.value.VARIANTs}"), "")
-        NAME          = try(file("${each.value.NAME}"), null)
-        POST_COMMAND  = try(data.template_file.DESTROY_POST_COMMAND_SCRIPT[each.key].rendered, "")
-    }
+#     template = <<-EOF
+#         ${try("${base64encode("${each.value.PRE_COMMAND}")}", "")}
+#     EOF
+# }
 
-    provisioner "local-exec" {
-        when = destroy
-        interpreter = ["bash", "-c"]
-        command = <<-EOF
-            %{ if self.triggers.PRE_COMMAND != "" ~}
-                echo "${self.triggers.PRE_COMMAND}" | base64 --decode | bash -s
-            %{ endif ~}
-            %{ if self.triggers.VARIANT != "" ~}
-                %{ for VARIANT in split(",", self.triggers.VARIANT) ~}
-                export ${VARIANT}
-                %{ endfor ~}
-            %{ endif ~}
-            %{ if self.triggers.NAME != null ~}
-                bash "${self.triggers.NAME}"
-            %{ endif ~}
-            %{ if self.triggers.POST_COMMAND != "" ~}
-                echo "${self.triggers.POST_COMMAND}" | base64 --decode | bash -s
-            %{ endif ~}
-        EOF 
-    }
-}
+# data "template_file" "DESTROY_POST_COMMAND_SCRIPT" {
+#     for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
+
+#     template = <<-EOF
+#         ${try("${base64encode("${each.value.POST_COMMAND}")}", "")}
+#     EOF
+# }
+
+# resource "null_resource" "EXECUTE_DESTROY_SCRIPT" {
+#     for_each = { for index, SCRIPT in var.DESTROY_SCRIPTs : index => SCRIPT }
+
+#     triggers = {
+#         always_run    = try("${each.value.ALWAYS}" == true ? timestamp() : null, null)
+#         PRE_COMMAND   = try(data.template_file.DESTROY_PRE_COMMAND_SCRIPT[each.key].rendered, "")
+#         VARIANT       = try(join(",", "${each.value.VARIANTs}"), "")
+#         NAME          = try(file("${each.value.NAME}"), null)
+#         POST_COMMAND  = try(data.template_file.DESTROY_POST_COMMAND_SCRIPT[each.key].rendered, "")
+#     }
+
+#     provisioner "local-exec" {
+#         when = destroy
+#         interpreter = ["bash", "-c"]
+#         command = <<-EOF
+#             %{ if self.triggers.PRE_COMMAND != "" ~}
+#                 echo "${self.triggers.PRE_COMMAND}" | base64 --decode | bash -s
+#             %{ endif ~}
+#             %{ if self.triggers.VARIANT != "" ~}
+#                 %{ for VARIANT in split(",", self.triggers.VARIANT) ~}
+#                 export ${VARIANT}
+#                 %{ endfor ~}
+#             %{ endif ~}
+#             %{ if self.triggers.NAME != null ~}
+#                 bash "${self.triggers.NAME}"
+#             %{ endif ~}
+#             %{ if self.triggers.POST_COMMAND != "" ~}
+#                 echo "${self.triggers.POST_COMMAND}" | base64 --decode | bash -s
+#             %{ endif ~}
+#         EOF 
+#     }
+# }
